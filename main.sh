1915bdd6671b8702fcb71c7b6dde22b6cecee546
Mohit Janbandhu <69210761+MJanbandhu@users.noreply.github.com>
2023-08-24 03:18:59 +0530

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,8 @@
+#cd ~/Calculator  # Change to your repository directory
+#!/bin/bash
+
+# Navigate to the directory containing the Python script
+#cd /path/to/Calculator  # Change to the correct path
+
+# Run the Python script
+python mainfile.py
